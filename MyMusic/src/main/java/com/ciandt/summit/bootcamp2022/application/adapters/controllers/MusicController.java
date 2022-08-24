c8ff0ca86837e52fb6af3c10ba144d19d45c7ff7
Jenifer <jnakabar@ciandt.com>
2022-08-24 09:45:48 -0300

    refect: adjust logger configuration file

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/MusicController.java
@@ -1,4 +1,5 @@
 package com.ciandt.summit.bootcamp2022.application.adapters.controllers;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/music")
 public class MusicController {
+
     private static Logger logger = LoggerFactory.getLogger(MusicController.class.getSimpleName());
-    public static void main(String[] args) {
-        logger.error("teste");
-    }
 
     @GetMapping
     public ResponseEntity<String> get() {
