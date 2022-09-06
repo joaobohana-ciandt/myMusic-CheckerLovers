1da272345df04c8a55eaa65704b514b662ca2ad7
joaovg@ciandt.com <joaovg@ciandt.com>
2022-09-06 09:35:37 -0300

    feature: implemented definition for user type

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domains/users/dto/UserDTO.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domains/users/dto/UserDTO.java
@@ -1,10 +1,11 @@
 package com.ciandt.summit.bootcamp2022.domains.users.dto;
 
 import com.ciandt.summit.bootcamp2022.domains.playlists.dtos.PlaylistDTO;
+import com.ciandt.summit.bootcamp2022.domains.userType.dto.UserTypeDTO;
 
 public class UserDTO {
 
-    public UserDTO(String id, String name, PlaylistDTO playlistDTO) {
+    public UserDTO(String id, String name, PlaylistDTO playlistDTO, UserTypeDTO userTypeDTO) {
 
     }
 }
